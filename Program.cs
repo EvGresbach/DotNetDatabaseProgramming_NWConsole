4b2d3792b088c5b520f1af1dfcff2873b2a20710
Evelynn Gresbach <egresbach1@my.wctc.edu>
2021-04-19 14:06:49 -0500

    Display all products from products

--- Program.cs
+++ Program.cs
@@ -22,6 +22,7 @@ namespace NorthwindConsole
                 string choice;
                 do
                 {
+                    Console.WriteLine("Menu\n"); 
                     //2
                     Console.WriteLine("1) Display all Categories");
                     Console.WriteLine("2) Add Category");
@@ -29,10 +30,10 @@ namespace NorthwindConsole
                     Console.WriteLine("4) Delete record from Categories"); //3
                     Console.WriteLine("5) Display specified Category and all related products");
                     Console.WriteLine("6) Display specified Category and active products");
-                    Console.WriteLine("7) Display all Categories and their related products");
+                    Console.WriteLine("7) Display all Categories and all related products");
                     Console.WriteLine("8) Display all Categories and active products");
                     //1
-                    Console.WriteLine("9) Display all Products");
+                    Console.WriteLine("9) Display Products");
                     Console.WriteLine("10) Display specified Product");
                     Console.WriteLine("11) Add new record to Products");
                     Console.WriteLine("12) Edit record from Products");
@@ -60,7 +61,7 @@ namespace NorthwindConsole
                         Console.ForegroundColor = ConsoleColor.White;
                     }
                     
-                    //Add record to category
+                    //2 Add record to category
                     else if (choice == "2")
                     {
                         Categories category = new Categories();
@@ -99,9 +100,10 @@ namespace NorthwindConsole
                         }
 
                     }
-
-                    //Display specified category + all rel products
-                    else if (choice == "3")
+                    //3) Edit record from Categories
+                    //4) Delete record from Categories
+                    //5 Display specified category + all rel products
+                    else if (choice == "5")
                     {
                         var db = new NWConsole_96_EXGContext();
                         var query = db.Categories.OrderBy(p => p.CategoryId);
@@ -123,9 +125,9 @@ namespace NorthwindConsole
                             Console.WriteLine(p.ProductName);
                         }
                     }
-
-                    //display all categories + all related products
-                    else if (choice == "4")
+                    //6) Display specified Category and active products
+                    //7) display all categories + all related products
+                    else if (choice == "7")
                     {
                         var db = new NWConsole_96_EXGContext();
                         var query = db.Categories.Include("Products").OrderBy(p => p.CategoryId);
@@ -138,6 +140,39 @@ namespace NorthwindConsole
                             }
                         }
                     }
+                    // 8) Display all Categories and active products
+                    // 9) Display Products
+                    else if (choice == "9"){
+                        Console.WriteLine("Display Products \n1) All Products \n2) Discontinued Products \n3) Active Products");
+                        string userChoice = Console.ReadLine(); 
+                        logger.Info($"Display Products - Option {choice} selected");
+
+                        var db = new NWConsole_96_EXGContext(); 
+                        if(userChoice == "1"){
+                            //display all products
+                            var query = db.Products.OrderBy(p => p.ProductId);
+                            foreach(var product in query){
+                                if(product.Discontinued)
+                                    Console.ForegroundColor = ConsoleColor.DarkRed; 
+                                else Console.ForegroundColor = ConsoleColor.White;
+                                Console.WriteLine($"{product.ProductName}");
+                            }
+                        }
+                        else if(userChoice == "2"){
+                            //display all discontinued
+                            
+                        }
+                        else if(userChoice == "3"){
+                            //display all active
+                        }
+                    }
+                    // 10) Display specified Products
+                    // 11) Add new record to Products
+                    // 12) Edit record from Products
+                    // 13) Delete record from Products
+
+
+
                     Console.WriteLine();
 
                 } while (choice.ToLower() != "q");
